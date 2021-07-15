e89e19232ea95bcf6151479f0742cff297a9d040
Смирнов Дмитрий Дмитриевич <d.smirnov@voskhod.ru>
2021-07-15 13:13:42 +0300

    +

--- src/main.go
+++ src/main.go
@@ -1,7 +1,15 @@
 package main
-
-import "fmt"
-
+import (
+	"fmt"
+	"net/http"
+)
+type msg string
+func (m msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	fmt.Println("Request: " + req.Method + " " + req.URL.String())
+	fmt.Fprint(resp, m)
+}
 func main() {
-	fmt.Println("Hello World!")
+	msgHandler := msg("Hello from Web Server in Go")
+	fmt.Println("Server is listening...")
+	http.ListenAndServe("localhost:8181", msgHandler)
 }
