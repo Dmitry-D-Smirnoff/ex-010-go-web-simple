5202d36ce62e3bd3718ee1ca727180ca83ae54d2
Смирнов Дмитрий Дмитриевич <d.smirnov@voskhod.ru>
2021-07-15 16:33:39 +0300

    +

--- src/main.go
+++ src/main.go
@@ -1,15 +1,84 @@
 package main
+
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 )
+type Book struct {
+	ID      string `json:"id"`
+	Title   string `json:"title"`
+	Author *Author `json:"author"`
+}
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
 type msg string
-func (m msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
-	fmt.Println("Request: " + req.Method + " " + req.URL.String())
-	fmt.Fprint(resp, m)
+var books []Book
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
+}
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(1000000))
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
+}
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"]
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(books)
+}
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
+}
+func basicResponse(resp http.ResponseWriter, req *http.Request) {
+	fmt.Println("Basic request: " + req.Method + " " + req.URL.String())
+	fmt.Fprint(resp, "Hello from Web Server in Go")
 }
 func main() {
-	msgHandler := msg("Hello from Web Server in Go")
-	fmt.Println("Server is listening...")
-	http.ListenAndServe("localhost:8181", msgHandler)
+	r := mux.NewRouter()
+	books = append(books, Book{ID: "1", Title: "Война и Мир", Author: &Author{Firstname: "Лев", Lastname: "Толстой"}})
+	books = append(books, Book{ID: "2", Title: "Преступление и наказание", Author: &Author{Firstname: "Фёдор", Lastname: "Достоевский"}})
+	r.HandleFunc("/", basicResponse).Methods("GET")
+	r.HandleFunc("/books", getBook).Methods("GET")
+	r.HandleFunc("/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/books", createBook).Methods("POST")
+	r.HandleFunc("/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
+	log.Fatal(http.ListenAndServe("localhost:8181", r))
 }
