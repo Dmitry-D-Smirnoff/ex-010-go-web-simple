38362fbcc5709759d707d7d1d279f2bd21bc2bec
Смирнов Дмитрий Дмитриевич <d.smirnov@voskhod.ru>
2021-08-04 22:10:01 +0300

    +

---
+++ go.mod
@@ -0,0 +1,7 @@
+module ex-010-go-web-simple
+
+go 1.16
+
+require (
+	github.com/gorilla/mux v1.8.0
+)
--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"./functions"
+	"ex-010-go-web-simple/functions"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"net/http/pprof"
 )
 func main() {
 	r := mux.NewRouter()
@@ -14,5 +15,23 @@ func main() {
 	r.HandleFunc("/books/", functions.CreateBook).Methods("POST")
 	r.HandleFunc("/books/{id}", functions.UpdateBook).Methods("PUT")
 	r.HandleFunc("/books/{id}", functions.DeleteBook).Methods("DELETE")
+
+	//Профилирование
+	//1. Создать нагрузку:
+	// > ab -n 1000000 -c10 http://localhost:8180/books/
+	//2. Сбор данных в файл
+	//2.1. Сохранение в файл:
+	// > wget http://127.0.0.1:8180/debug/pprof/profile?seconds=5 -O profile
+	//2.2. Просмотр файла-результата:
+	// > go tool pprof profile
+	//3. Прямой сбор данных
+	// > go tool pprof ex-010-go-web-simple http://127.0.0.1:8180/debug/pprof/profile
+	//4. Внутри средства вызвать генерацию диаграмм:
+	// > > web
+	r.HandleFunc("/debug/pprof/", pprof.Index)
+	r.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	r.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	r.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	r.HandleFunc("/debug/pprof/trace", pprof.Trace)
 	log.Fatal(http.ListenAndServe("localhost:8180", r))
 }
